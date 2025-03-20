fcd932499231a0c6cbadf0002347a81f6c4349de
HafeezX3445 <hafeezshaik245@gmail.com>
2025-03-20 12:28:34 +0530

    Added DataTypes Class file

---
+++ VariablesExample/Concepts/DataTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesExample.Concepts
+{
+    public class DataTypes
+    {
+
+        // Add a method to return the definition of Data Types
+        public string GetDefinition()
+        {
+            return "";
+        }
+
+        // Method to return the list of Data Types with definitions
+
+        // Examples for each Data Type
+
+    }
+}
